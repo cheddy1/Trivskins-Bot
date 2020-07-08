4ca6b77a5d3fd8789ece09b9f825792a9b5ed17b
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2020-07-08 14:42:19 -0500

    Update value.py

--- cogs/value.py
+++ cogs/value.py
@@ -2,8 +2,8 @@ import discord
 import requests
 from discord.ext import commands
 
-steamapi = '' #steam api
-key = '' CSGO backpack api
+steamapi = ''
+key = ''
 class Value(commands.Cog, name="Inventory Value Commands"):
   def __init__(self, bot):
         self.bot = bot
