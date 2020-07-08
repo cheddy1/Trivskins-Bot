f84e6f35e0d6a22568e472c910947ea384c24c46
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2020-07-08 14:41:35 -0500

    Update template.py

--- cogs/template.py
+++ cogs/template.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 
-tradelogging = #logging channel id
-tradelow = #low tier trade channel id
-tradehigh = #high tier trade channel id
-tradeknife = #knife trade channel id
+tradelogging = 
+tradelow = 
+tradehigh = 
+tradeknife = 
+tradetemplate = ''
 
 class Template(commands.Cog, name="Template Follower"):
   def __init__(self, bot):
@@ -17,25 +18,22 @@ class Template(commands.Cog, name="Template Follower"):
     else:
         logging = guild.get_channel(tradelogging) #Trade logging
         banned_list = ['cash','paypal','steam card','gift card', 'bitcoin','btc','crypto','steam gift card','Cash','Paypal','PayPal','Steam card','Steam Card','Gift card','Gift Card', 'Bitcoin','BitCoin','BTC','Crypto','Steam gift card']
-        if (ctx.channel.id == tradelow) or (ctx.channel.id == 570411660548898836) or (ctx.channel.id == 705548750097088562): #Trade ad channels
-          if ("[H]" not in ctx.content) or ("[W]" not in ctx.content) or ("[TL]" not in ctx.content):
-            if ctx.author.bot:
-                pass
+        if (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh) or (ctx.channel.id == tradeknife): #Trade ad channels
+            if ("[H]" not in ctx.content) or ("[W]" not in ctx.content) or (("steamcommunity.com/tradeoffer" not in ctx.content) and ("[TL]" not in ctx.content)):
+                if ctx.author.bot:
+                    pass
+                else:
+                    await logging.send("Sorry "+ctx.author.mention+", but your trade ad didn't follow our required template, and therefore deleted. Please refer to <#"+tradetemplate+"> before posting again.\n")
+                await ctx.delete()
+            elif [ele for ele in banned_list if(ele in ctx.content)] :
+                if ctx.author.bot:
+                    pass
+                else:
+                    await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). Please refer to the list of banned words and phrases in <#"+tradetemplate+"> before posting again.\n")
+                await ctx.delete()
             else:
-                await logging.send("Sorry "+ctx.author.mention+", but your trade ad didn't follow our required template, and therefore deleted. Please refer to <#705548750097088562> before posting again.\n")
-            await ctx.delete()
-          elif [ele for ele in banned_list if(ele in ctx.content)] :
-            if ctx.author.bot:
-                pass
-            else:
-                await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). Please refer to the list of banned words and phrases in <#705548750097088562> before posting again.\n")
-            await ctx.delete()
-          else:
-            pass
+              pass
         else:
           pass
-
-
-
 def setup(bot):
     bot.add_cog(Template(bot))
