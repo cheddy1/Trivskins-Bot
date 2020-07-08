b14653e64ef149aad5bd201cec59007431609f3b
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2020-07-08 14:39:26 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -11,12 +11,17 @@ class General(commands.Cog, name="General Commands"):
   async def invite(self, ctx): #Pulls up an invite link.
       await ctx.message.channel.send("<https://cheddydev.com/discord>")
 
+  @commands.command(name='report')
+  async def report(self, ctx): #Helps a user report.
+      await ctx.message.delete()
+      await ctx.message.channel.send("Need to report someone? Open a modmail with !dm")
+
   @commands.command(name='help')
   async def help(self, ctx): #Help command
       desc = 'Thanks for using the Trivskins Trading Bot, coded and created by cheddy#7744. Please contact him if you have any issues. '
-      features1 = 'This bot has many features, all pertaining to CS:GO trading. They include:\n\n-Screenshots from steam inspect links.\n-CS:GO inventory value from their steam profile link.\n-Allow users to recieve roles based on their CS:GO inventory value.\n-Give the average steam market value of a skin.'
-      commands1 = '*These commands may only only be used in their respective channels.*\n\n**!help** *Pull up this help menu.*\n**!verify** *Gives user a value role.*\n**!value "Steam Profile Link"** *Gives the CS:GO inventory value of a steam profile. *\n**!pc "Weapon" "Skin" "(Wear)"** *Price check a skin.*\n**!invite** *Get the Discord invite link.* \n**!support** *Help support the hosting of this bot.*'
-      embed = discord.Embed(title="Trvskins Trading Bot Help", description=desc, url='https://cheddydev.com/', color=0X2C3675)
+      features1 = 'This bot has many features, please take a look at the commands available to you below.'
+      commands1 = '**!dm** *Report someone/get server support.*\n**!value "Steam Profile Link"** *Gives the CS:GO inventory value of a steam profile.* \n**!pc "Weapon" "Skin" "Wear"** *Price check a skin.*\n**!invite** *Get the Discord invite link.* \n**!support** *Help support the hosting of this bot.***\n**!help** *Pull up this help menu.*'
+      embed = discord.Embed(title="Trivskins Trading Bot Help", description=desc, url='https://cheddydev.com/', color=0X2C3675)
       embed.add_field(name="Features", value=features1, inline=False)
       embed.add_field(name="Commands", value=commands1, inline=False) 
       await ctx.message.channel.send(embed=embed)
