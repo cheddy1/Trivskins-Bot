4fa0d79558d848f46c1bb8e923a37c99707167c2
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-01-07 16:47:03 -0600

    Update template.py

--- cogs/template.py
+++ cogs/template.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import re
 
 tradelogging = 
 tradelow = 
@@ -17,7 +18,7 @@ class Template(commands.Cog, name="Template Follower"):
         return
     else:
         logging = guild.get_channel(tradelogging) #Trade logging
-        banned_list = ['cash','paypal','steam card','gift card', 'bitcoin','btc','crypto','steam gift card','Cash','Paypal','PayPal','Steam card','Steam Card','Gift card','Gift Card', 'Bitcoin','BitCoin','BTC','Crypto','Steam gift card']
+        banned_list = ['cash','paypal','steam card','gift card', 'bitcoin','btc','crypto','steam gift card','tf2','team fortress','alipay','bank transfer','eth','qq']
         if (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh) or (ctx.channel.id == tradeknife): #Trade ad channels
             if ("[H]" not in ctx.content) or ("[W]" not in ctx.content) or (("steamcommunity.com/tradeoffer" not in ctx.content) and ("[TL]" not in ctx.content)):
                 if ctx.author.bot:
@@ -25,12 +26,18 @@ class Template(commands.Cog, name="Template Follower"):
                 else:
                     await logging.send("Sorry "+ctx.author.mention+", but your trade ad didn't follow our required template, and therefore deleted. Please refer to <#"+tradetemplate+"> before posting again.\n")
                 await ctx.delete()
-            elif [ele for ele in banned_list if(ele in ctx.content)] :
+            elif re.compile('|'.join(banned_list),re.IGNORECASE).search(ctx.content):
                 if ctx.author.bot:
                     pass
                 else:
-                    await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). Please refer to the list of banned words and phrases in <#"+tradetemplate+"> before posting again.\n")
-                await ctx.delete()
+                    words = re.compile('|'.join(banned_list),re.IGNORECASE).findall(ctx.content)
+                    word = ', '.join(words)
+                    await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). You said **"+word+ "** which is currently not allowed according to our rules. \nPlease refer to the list of banned words and phrases in <#"+tradetemplate+"> before posting again.\n")
+                    await ctx.delete()
+            elif (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh):
+                if (ctx.content.find('knife') < ctx.content.find('[W]') and ctx.content.find('knife') > 0) or (ctx.content.find('Knife') < ctx.content.find('[W]') and ctx.content.find('Knife') > 0):
+                    await logging.send("Bad puppy, "+ctx.author.mention+"! Looks like you had a knife in your **[H]** block. Please post the knife as a separate ad in <#"+str(tradeknife)+">.\n")
+                    await ctx.delete()
             else:
               pass
         else:
