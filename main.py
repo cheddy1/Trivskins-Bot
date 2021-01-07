582ba6da3c06bb4a7026142dfbc2ed22bc494592
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-01-07 16:49:21 -0600

    Added support for new 1.6 rules feature.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import discord
 import asyncio
 from discord.ext import commands
 import sys, traceback
-token = '
+token = ''
+intents = discord.Intents.all()
+intents.members = True
 
 startup_extensions = ['cogs.value','cogs.general','cogs.modmail','cogs.template','cogs.pricecheck','cogs.screenshot']
 
-bot = commands.Bot(command_prefix ='!')
+bot = commands.Bot(command_prefix ='!', intents=intents)
 bot.remove_command('help')
 if __name__ == '__main__':
     for extension in startup_extensions:
@@ -21,8 +23,23 @@ async def on_ready(): #Runs when the bot connects.
         await bot.change_presence(activity=discord.Game(name='!help | v1.0.3',type=1))
         await asyncio.sleep(120)
         await bot.change_presence(activity=discord.Activity(name='!dm for support', type=3))
-        await asyncio.sleep(120)
+        await asyncio.sleep(60)
         #await bot.change_presence(activity=discord.Activity(name='cheddydev.com', type=3))
         #await client.change_presence(activity=discord.Streaming(name="FOLLOW MY TWITCH", url="https://twitch.tv/cheddyGG", type=1))
         #await asyncio.sleep(30)
+
+@bot.event
+async def on_member_update(before, after):
+    if (before.pending) == True:
+      if (after.pending) == False:
+        role = discord.utils.get(after.guild.roles, name="Trader")
+        role2 = discord.utils.get(after.guild.roles, name="↥─────《 Server Rank 》────↥")
+        role3 = discord.utils.get(after.guild.roles, name="↥────《 Inventory Value 》───↥")
+        role4 = discord.utils.get(after.guild.roles, name="↥──────《 Self Role 》──────↥")
+        await after.add_roles(role)
+        await after.add_roles(role2)
+        await after.add_roles(role3)
+        await after.add_roles(role4)
 bot.run(token)
+
+
