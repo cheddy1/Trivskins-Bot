7f9347e29512a26836a58d214ae14a7abfd7cfda
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-02-16 16:17:06 -0600

    csgo.gallery udpates

--- cogs/screenshot.py
+++ cogs/screenshot.py
@@ -1,16 +1,14 @@
-#This is currently unused, as csgo.gallery has been under maintenance for quite some time.
-
 import discord
 import aiohttp
 import asyncio
 import requests
 import datetime
 from discord.ext import commands
-
+screenshotchannel = 569901952545325056
 class screenshot(commands.Cog, name="Screenshot"):
   def __init__(self, bot):
       self.bot = bot
-  
+    
   @commands.Cog.listener()
   async def on_message(self, ctx):
     guild = ctx.guild
@@ -18,25 +16,24 @@ class screenshot(commands.Cog, name="Screenshot"):
         return
     else:
       global data
-      global url
       global threeD
       async def getdata():
           global floatvalue
           global rarityname
           global color1
           global weaponname
-          global sticker0
-          global sticker1
-          global sticker2
-          global sticker3
-          global sticker4
+          global stickerRaw1
+          global stickerRaw2
+          global stickerRaw3
+          global stickerRaw4
+          global stickerRaw5
           global nostickers
-          global numberofstickers
-          global stickerurl0
-          global stickerurl1
-          global stickerurl2
-          global stickerurl3
-          global stickerurl4
+          global numberOfStickers
+          global stickerUrl1
+          global stickerUrl2
+          global stickerUrl3
+          global stickerUrl4
+          global stickerUrl5
           global paintseed
           global defindex
           global paintindex
@@ -44,7 +41,7 @@ class screenshot(commands.Cog, name="Screenshot"):
           color1 = 0X02FE0E
           resp = requests.get(floatraw)
           rawdata1 = resp.content.decode()
-          stickerdata = rawdata1.split(',')
+          stickerData = rawdata1.split(',')
           rawdata2 = rawdata1.replace(',',' ')
           rawdata = rawdata2.split()
           #Getting the weapon and skin name.
@@ -53,13 +50,10 @@ class screenshot(commands.Cog, name="Screenshot"):
           weaponskinindex = int(weaponskinindex2)
           weaponskin1 = rawdata[weaponskinindex:]
           weaponname1 = ' '.join(weaponskin1)
-          weaponname2 = weaponname1.replace('\"full_item_name\":\"','')
-          weaponname3 = weaponname2.replace('\"','')
-          weaponname = weaponname3.replace('}','')
+          weaponname = weaponname1.replace('\"full_item_name\":\"','').replace('\"','').replace('}','')
           steamnameurl = 'https://steamcommunity.com/market/listings/730/'
-          weaponurlbasic1 = weaponname.replace(')','%29')
-          weaponurlbasic = weaponurlbasic1.replace(' ','%20')
-          weaponurl = steamnameurl+weaponurlbasic
+          weaponUrlRaw = weaponname.replace(')','%29').replace(' ','%20')
+          weaponurl = steamnameurl+weaponUrlRaw
           #Getting the paint seed, paint index, and defindex.
           paintseedindex1 = [i for i, elem in enumerate(rawdata) if 'paintseed' in elem]
           paintseedindex2 = ''.join([str(i) for i in paintseedindex1])
@@ -88,68 +82,43 @@ class screenshot(commands.Cog, name="Screenshot"):
           rarityname = rarityname2.replace('\"','')
           if rarityname == 'Consumer':
               color1 = 0XC2C2C2
-          if rarityname == 'Industrial':
+          elif rarityname == 'Industrial':
               color1 = 0X77A5FF
-          if rarityname == 'Mil-Spec':
+          elif rarityname == 'Mil-Spec':
               color1 = 0X3C66DA
-          if rarityname == 'Restricted':
+          elif rarityname == 'Restricted':
               color1 = 0X893AE7
-          if rarityname == 'Classified':
+          elif rarityname == 'Classified':
               color1 = 0XD437D2
-          if rarityname == 'Covert':
+          elif rarityname == 'Covert':
               color1 = 0XE81E02
-          if rarityname == 'Contraband':
+          else:
               color1 = 0XB88132
           #Getting the stickers.
-          stickerindex1 = [i for i, elem in enumerate(stickerdata) if '\"name\"' in elem]
-          numberofstickers = len(stickerindex1)
-          if numberofstickers == 0:
+          stickerIndex = [i for i, elem in enumerate(stickerData) if '\"name\"' in elem]
+          numberOfStickers = len(stickerIndex)
+          if numberOfStickers == 0:
               nostickers = True
               pass
           else:
               nostickers = False
               try:
-                  steamstickerurl = 'https://steamcommunity.com/market/listings/730/Sticker%20%7C%20'
-                  stickerindex = int(stickerindex1[0])
-                  stickers1 = stickerdata[stickerindex]
-                  stickers2 = stickers1.replace('\"name\":\"','')
-                  stickers3 = stickers2.replace(']','')
-                  sticker0 = stickers3.replace('\"}','')
-                  sticker00 = sticker0.replace(')','%29')
-                  stickerreplace = sticker00.replace(' ','%20')
-                  stickerurl0 = steamstickerurl+stickerreplace
-                  stickerindex = int(stickerindex1[1])
-                  stickers1 = stickerdata[stickerindex]
-                  stickers2 = stickers1.replace('\"name\":\"','')
-                  stickers3 = stickers2.replace(']','')
-                  sticker1 = stickers3.replace('\"}','')
-                  sticker11 = sticker1.replace(')','%29')
-                  stickerreplace = sticker11.replace(' ','%20')
-                  stickerurl1 = steamstickerurl+stickerreplace
-                  stickerindex = int(stickerindex1[2])
-                  stickers1 = stickerdata[stickerindex]
-                  stickers2 = stickers1.replace('\"name\":\"','')
-                  stickers3 = stickers2.replace(']','')
-                  sticker2 = stickers3.replace('\"}','')
-                  sticker22 = sticker2.replace(')','%29')
-                  stickerreplace = sticker22.replace(' ','%20')
-                  stickerurl2 = steamstickerurl+stickerreplace
-                  stickerindex = int(stickerindex1[3])
-                  stickers1 = stickerdata[stickerindex]
-                  stickers2 = stickers1.replace('\"name\":\"','')
-                  stickers3 = stickers2.replace(']','')
-                  sticker3 = stickers3.replace('\"}','')
-                  sticker33 = sticker3.replace(')','%29')
-                  stickerreplace = sticker33.replace(' ','%20')
-                  stickerurl3 = steamstickerurl+stickerreplace
-                  stickerindex = int(stickerindex1[4])
-                  stickers1 = stickerdata[stickerindex]
-                  stickers2 = stickers1.replace('\"name\":\"','')
-                  stickers3 = stickers2.replace(']','')
-                  sticker4 = stickers3.replace('\"}','')
-                  sticker44 = sticker4.replace(')','%29')
-                  stickerreplace = sticker44.replace(' ','%20')
-                  stickerurl4 = steamstickerurl+stickerreplace
+                  steamStickerUrl = 'https://steamcommunity.com/market/listings/730/Sticker%20%7C%20'
+                  stickerIndex1 = int(stickerIndex[0])
+                  stickerRaw1 = stickerData[stickerIndex1].replace('\"name\":\"','').replace(']','').replace('\"}','')
+                  stickerUrl1 = steamStickerUrl+stickerRaw1.replace(')','%29').replace(' ','%20')
+                  stickerIndex2 = int(stickerIndex[1])
+                  stickerRaw2 = stickerData[stickerIndex2].replace('\"name\":\"','').replace(']','').replace('\"}','')
+                  stickerUrl2 = steamStickerUrl+stickerRaw2.replace(')','%29').replace(' ','%20')
+                  stickerIndex3 = int(stickerIndex[2])
+                  stickerRaw3 = stickerData[stickerIndex3].replace('\"name\":\"','').replace(']','').replace('\"}','')
+                  stickerUrl3 = steamStickerUrl+stickerRaw3.replace(')','%29').replace(' ','%20')
+                  stickerIndex4 = int(stickerIndex[3])
+                  stickerRaw4 = stickerData[stickerIndex4].replace('\"name\":\"','').replace(']','').replace('\"}','')
+                  stickerUrl4 = steamStickerUrl+stickerRaw4.replace(')','%29').replace(' ','%20')
+                  stickerIndex5 = int(stickerIndex[4])
+                  stickerRaw5 = stickerData[stickerIndex5].replace('\"name\":\"','').replace(']','').replace('\"}','')
+                  stickerUrl5 = steamStickerUrl+stickerRaw5.replace(')','%29').replace(' ','%20')
               except:
                   pass
           #Getting the float value.
@@ -159,10 +128,10 @@ class screenshot(commands.Cog, name="Screenshot"):
           floatvalue1 = rawdata[floatindex]
           floatvalue = floatvalue1.replace('\"floatvalue\":','')
           
-      async def getimage(): #Gets the screenshot from csgo.gallery.
+      async def getimage(url): #Gets the screenshot from csgo.gallery.
         global floatvalue
-        async with aiohttp.ClientSession() as session:   
-            async with session.get(url) as resp:
+        async with aiohttp.ClientSession() as session:  
+            async with session.post(url) as resp:
               #TODO: Add more data values, add weapon thumbnail to the embed.
               if nostickers == True:
                   desc = ctx.author.mention + ', here\'s your screenshot.'
@@ -175,18 +144,17 @@ class screenshot(commands.Cog, name="Screenshot"):
                   embed.set_footer(text="Screenshot Taken:", icon_url="https://cdn.discordapp.com/avatars/217440011451105280/7752b9953c981fe9b072dd0949e956f4.png?size=128")
                   msg = await ctx.channel.send(embed=embed)
                   msg
-                  await asyncio.sleep(2)
               else:
-                  if numberofstickers == 1:
-                      stickers = '**['+sticker0+']('+stickerurl0+')**'
-                  if numberofstickers == 2:
-                      stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**'
-                  if numberofstickers == 3:
-                      stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**'
-                  if numberofstickers == 4:
-                      stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**\n**['+sticker3+']('+stickerurl3+')**'
-                  if numberofstickers == 5:
-                      stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**\n**['+sticker3+']('+stickerurl3+')**\n**['+sticker4+']('+stickerurl4+')**'
+                  if numberOfStickers == 1:
+                      stickers = '**['+stickerRaw1+']('+stickerUrl1+')**'
+                  if numberOfStickers == 2:
+                      stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**'
+                  if numberOfStickers == 3:
+                      stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**'
+                  if numberOfStickers == 4:
+                      stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**\n**['+stickerRaw4+']('+stickerUrl4+')**'
+                  if numberOfStickers == 5:
+                      stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**\n**['+stickerRaw4+']('+stickerUrl4+')**\n**['+stickerRaw5+']('+stickerUrl5+')**'
                   desc = ctx.author.mention + ', here\'s your screenshot.'
                   embed = discord.Embed(title="CS:GO Skin Screenshot", description=desc, url='https://cheddydev.com/', color=color1, timestamp = datetime.datetime.utcnow())
                   linkz= '[Screenshot]('+url+')'
@@ -211,16 +179,16 @@ class screenshot(commands.Cog, name="Screenshot"):
                       newembed.set_footer(text="Screenshot Taken:", icon_url="https://cdn.discordapp.com/avatars/217440011451105280/7752b9953c981fe9b072dd0949e956f4.png?size=128")
                       await msg.edit(embed=newembed)
                   else:
-                      if numberofstickers == 1:
-                          stickers = '**['+sticker0+']('+stickerurl0+')**'
-                      if numberofstickers == 2:
-                          stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**'
-                      if numberofstickers == 3:
-                          stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**'
-                      if numberofstickers == 4:
-                          stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**\n**['+sticker3+']('+stickerurl3+')**'
-                      if numberofstickers == 5:
-                          stickers = '**['+sticker0+']('+stickerurl0+')**\n**['+sticker1+']('+stickerurl1+')**\n**['+sticker2+']('+stickerurl2+')**\n**['+sticker3+']('+stickerurl3+')**\n**['+sticker4+']('+stickerurl4+')**'
+                      if numberOfStickers == 1:
+                          stickers = '**['+stickerRaw1+']('+stickerUrl1+')**'
+                      if numberOfStickers == 2:
+                          stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**'
+                      if numberOfStickers == 3:
+                          stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**'
+                      if numberOfStickers == 4:
+                          stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**\n**['+stickerRaw4+']('+stickerUrl4+')**'
+                      if numberOfStickers == 5:
+                          stickers = '**['+stickerRaw1+']('+stickerUrl1+')**\n**['+stickerRaw2+']('+stickerUrl2+')**\n**['+stickerRaw3+']('+stickerUrl3+')**\n**['+stickerRaw4+']('+stickerUrl4+')**\n**['+stickerRaw5+']('+stickerUrl5+')**'
                       desc = ctx.author.mention + ', here\'s your screenshot.'
                       newembed = discord.Embed(title="CS:GO Skin Screenshot", description=desc, url='https://cheddydev.com/', color=color1, timestamp = datetime.datetime.utcnow())
                       linkz= '[Screenshot]('+url+')'
@@ -232,12 +200,14 @@ class screenshot(commands.Cog, name="Screenshot"):
                       newembed.set_footer(text="Screenshot Taken:", icon_url="https://cdn.discordapp.com/avatars/217440011451105280/7752b9953c981fe9b072dd0949e956f4.png?size=128")
                       await msg.edit(embed=newembed)
 
-      if ctx.channel.id == 494905423393062912: #Checks inspect link, if its good then go to get the image.
+
+      if ctx.channel.id == screenshotchannel: #Checks inspect link, if its good then go to get the image.
           if ctx.author == self.bot.user:
             pass
           else:
             if ctx.content.startswith("steam://rungame/730/76561202255233023/+csgo_econ_action_preview"):
               global inspect
+              global url
               floatwebsite = "https://api.csgofloat.com/?url="
               screenwebsite = "https://csgo.gallery/" 
               inspect = ctx.content
@@ -248,7 +218,7 @@ class screenshot(commands.Cog, name="Screenshot"):
               await ctx.channel.send('Getting your screenshot, '+ ctx.author.mention+ ', please wait.',delete_after=15.0)
               async with ctx.channel.typing():
                   await getdata()
-                  await getimage()
+                  await getimage(url)
             else:
               await ctx.delete()
               await ctx.channel.send('That is not a valid inspect link, ' + ctx.author.mention +'!',delete_after=8.0)
