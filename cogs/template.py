6eaa635f0d612233c0bc70cde5d758511cd2035b
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-04-20 12:59:54 -0500

    added spam filter

--- cogs/template.py
+++ cogs/template.py
@@ -32,34 +32,44 @@ class Template(commands.Cog, name="Forced Template"):
         
         # Trade ad channels
         if (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh) or (ctx.channel.id == tradeknife):
-            
+
+            #Makes sure the user is not spamming the channel by checking who was the last one to post
+            fetchID = await ctx.channel.history(limit = 2).flatten()
+            if (fetchID[1].author == ctx.author):
+                if ctx.author.bot:
+                    pass
+                else:
+                    await ctx.delete()
+                    await logging.send(ctx.author.mention+", please wait until someone else posts an ad before posting yours again. Please refer to <#"+tradetemplate+"> for our rules.\n")
+
+            else: 
             # Ad tempplate checker
-            if ("[H]" not in ctx.content) or ("[W]" not in ctx.content) or (("steamcommunity.com/tradeoffer" not in ctx.content) and ("[TL]" not in ctx.content)):
-                if ctx.author.bot:
-                    pass
-                else:
-                    await logging.send("Sorry "+ctx.author.mention+", but your trade ad didn't follow our required template, and therefore deleted. Please refer to <#"+tradetemplate+"> before posting again.\n")
-                await ctx.delete()
-            
-            # Banned word checker
-            elif re.compile('|'.join(banned_list),re.IGNORECASE).search(ctx.content):
-                if ctx.author.bot:
-                    pass
-                else:
-                    words = re.compile('|'.join(banned_list),re.IGNORECASE).findall(ctx.content)
-                    word = ', '.join(words)
-                    await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). You said **"+word+ "** which is currently not allowed according to our rules. \nPlease refer to the list of banned words and phrases in <#"+tradetemplate+"> before posting again.\n")
+                if ("[H]" not in ctx.content) or ("[W]" not in ctx.content) or (("steamcommunity.com/tradeoffer" not in ctx.content) and ("[TL]" not in ctx.content)):
+                    if ctx.author.bot:
+                        pass
+                    else:
+                        await logging.send("Sorry "+ctx.author.mention+", but your trade ad didn't follow our required template, and therefore deleted. Please refer to <#"+tradetemplate+"> before posting again.\n")
                     await ctx.delete()
-            
-            # If the ad passes all other checks, make sure that the ad does not have a knife in the incorrect channel
-            elif (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh):
-                ad = ctx.content.lower()
-                if (ad.find('knife') < ad.find('[w]') and ad.find('knife') > 0) or (ad.find('daggers') < ad.find('[w]') and ad.find('daggers') > 0) or (ad.find('bayonet') < ad.find('[w]') and ad.find('bayonet') > 0) or (ad.find('karambit') < ad.find('[w]') and ad.find('karambit') > 0):
-                    await logging.send("Bad puppy, "+ctx.author.mention+"! Looks like you had a knife in your **[H]** block. Please post the knife as a separate ad in <#"+str(tradeknife)+">.\n")
-                    await ctx.delete()
-            else:
-              pass
-        else:
-          pass
+                
+                # Banned word checker
+                elif re.compile('|'.join(banned_list),re.IGNORECASE).search(ctx.content):
+                    if ctx.author.bot:
+                        pass
+                    else:
+                        words = re.compile('|'.join(banned_list),re.IGNORECASE).findall(ctx.content)
+                        word = ', '.join(words)
+                        await logging.send("Sorry "+ctx.author.mention+", but your ad included banned word(s) or phrase(s). You said **"+word+ "** which is currently not allowed according to our rules. \nPlease refer to the list of banned words and phrases in <#"+tradetemplate+"> before posting again.\n")
+                        await ctx.delete()
+                
+                # If the ad passes all other checks, make sure that the ad does not have a knife in the incorrect channel
+                elif (ctx.channel.id == tradelow) or (ctx.channel.id == tradehigh):
+                    ad = ctx.content.lower()
+                    if (ad.find('knife') < ad.find('[w]') and ad.find('knife') > 0) or (ad.find('daggers') < ad.find('[w]') and ad.find('daggers') > 0) or (ad.find('bayonet') < ad.find('[w]') and ad.find('bayonet') > 0) or (ad.find('karambit') < ad.find('[w]') and ad.find('karambit') > 0) or (ad.find('glove') < ad.find('[w]') and ad.find('glove') > 0):
+                        await logging.send("Bad puppy, "+ctx.author.mention+"! Looks like you had a knife/glove in your **[H]** block. Please post the knife/glove as a separate ad in <#"+str(tradeknife)+">.\n")
+                        await ctx.delete()
+                
+                else:
+                  pass
+
 def setup(bot):
     bot.add_cog(Template(bot))
