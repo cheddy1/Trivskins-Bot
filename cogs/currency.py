c53fd98a1dff95b83a9ee394af1d22d4d10e5b5a
Christian Edwards <49890784+cheddy1@users.noreply.github.com>
2021-07-18 23:44:39 -0500

    added ticket buy and leaderboard commands

--- cogs/currency.py
+++ cogs/currency.py
@@ -1,12 +1,14 @@
 import discord
 import sqlite3
+import random
 import time
+import asyncio
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions, MissingRequiredArgument
+import difflib
 
 connection = sqlite3.connect('Trivskins.db')
 cursor = connection.cursor()
-
 gain_rate = 60  # how frequently users can gain coins in seconds
 gain_amount = 1  # how many coins users gain per rate
 
@@ -26,19 +28,17 @@ class Currency(commands.Cog, name="Currency"):
         self.bot = bot
         self.connection = sqlite3.connect("Trivskins.db")
         self.c = self.connection.cursor()
+        self.c.execute('''DROP TABLE users''')
         self.c.execute('''CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY, name text NOT NULL, xp integer NOT NULL, xp_time integer NOT NULL)''')
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
         guild = ctx.guild
-        if guild is None:
+        if guild is None or ctx.author == self.bot.user or ctx.author.bot:
             return
-        if ctx.author == self.bot.user:
-            return
-
         user_id = int(ctx.author.id)
         name = str(ctx.author)
-        xp = 0
+        xp = 1
         xp_time = int(time.time())
 
         # If this is the users first time chatting, insert them into the table
@@ -99,14 +99,116 @@ class Currency(commands.Cog, name="Currency"):
             embed.add_field(name="Shop", value=embed_shop, inline=False)
         await ctx.message.channel.send(embed=embed)
 
+    @commands.command(aliases=['lb'])
+    async def leaderboard(self, ctx):
+        all_user_list = self.c.execute('''SELECT name, xp FROM users ORDER BY xp DESC''').fetchall()
+        embed_desc = ''
+        ten_count = 1
+        total_count = 0
+        place_count = 0
+        continue_counting = True
+        top_ten_list = all_user_list[:10]
+        for user in top_ten_list:
+            embed_desc += str(ten_count) + '. ' + user[0] + ' - **' + str(user[1]) + '** \U0001FA99 \n'
+            ten_count += 1
+        for user in all_user_list:
+            total_count += 1
+            if user[0] == str(ctx.message.author) and continue_counting:
+                place_count = total_count
+                continue_counting = False
+
+        embed = discord.Embed(title="Coins Leaderboard", description=embed_desc, color=0Xfcba03)
+        embed.set_footer(text='Your rank: ' + str(place_count) + '/' + str(total_count))
+        await ctx.message.channel.send(embed=embed)
+
+    @commands.command(aliases=['hl'])
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def highlow(self, ctx, *args):
+        if not args:
+            await ctx.message.add_reaction('\U0000274C')
+            return await ctx.message.channel.send("Missing amount of coins to gamble. `!coinflip (amount)`")
+        else:
+            seed = random.randint(0, 100)
+            embed_desc = 'M number number is **__' + str(seed) + '__**\nPress \U00002B06 if you think your number will be higher\nPress \U00002B07 if you think your number will be lower'
+            embed = discord.Embed(title="High Low", description=embed_desc, color=0Xfcba03)
+            embed_highlow = await ctx.message.channel.send(embed=embed)
+            await embed_highlow.add_reaction('\U00002B06')
+            await embed_highlow.add_reaction('\U00002B07')
+
+            try:
+                userchoice, user = await self.bot.wait_for('reaction_add', check=lambda message: message.author == ctx.author, timeout=30)
+            except asyncio.TimeoutError:
+                await embed_highlow.add_reaction
+                await ctx.message.channel.send("Timed out!")
+                return
+
+
     # Shop command to allow users to spend their coins
     @commands.command()
     async def buy(self, ctx, item, amount, *args):
-
-        if args[0] == 'ticket' and len(args) == 3:
-
-            await ctx.message.channel.send("Sorry but giveaways have not yet been enabled. Please check back soon!")
-
+        if item == 'ticket':
+            try:
+                amount = int(amount)
+            except ValueError:
+                await ctx.message.add_reaction('\U0000274C')
+                return await ctx.message.channel.send("Invalid input. Try `!buy ticket (amount) (giveaway name)`")
+            giveaways = self.c.execute('''SELECT name FROM giveaways''').fetchall()
+            if len(giveaways) > 1:
+                result = ', '.join(item[0] for item in giveaways)
+                if args:
+                    giveaway_name = ' '.join(args)
+                    giveaway_name = str(difflib.get_close_matches(giveaway_name, result.split(","), 1, .4)[0]).strip()
+                else:
+                    await ctx.message.add_reaction('\U0000274C')
+                    return await ctx.message.channel.send("You must specify the giveaway name because there are multiple giveaways running currently. Use `!buy ticket (amount) (giveaway name)`\nCurrently open: **" + result+ "**")
+            elif len(giveaways) == 1:
+                giveaway_name = giveaways[0][0]
+            else:
+                await ctx.message.channel.send("There are no giveaways currently!")
+                return await ctx.message.add_reaction('\U0000274C')
+            if not giveaway_name:
+                await ctx.message.add_reaction('\U0000274C')
+                return await ctx.message.channel.send("A giveaway with that name does not exist! Currently running giveaways: " + ', '.join(giveaways))
+            user_id = ctx.message.author.id
+            giveaway_info = self.c.execute('''SELECT ticket_cost, ticket_max FROM giveaways WHERE name=?''', (giveaway_name,)).fetchone()
+            user_tickets = self.c.execute('''SELECT tickets FROM entries WHERE name=? AND id=?''', (giveaway_name, user_id)).fetchone()
+            total_cost = amount * int(giveaway_info[0])
+            current_bal = self.c.execute('''SELECT xp FROM users WHERE id=?''', (user_id,)).fetchone()
+            while True:
+                if not current_bal:
+                    await ctx.message.add_reaction('\U0000274C')
+                    return await ctx.message.channel.send("Please send a message before attempting to buy, you haven't been added to my database yet!")
+                if total_cost > current_bal[0]:
+                    total_cost = total_cost - int(giveaway_info[0])
+                    amount = amount - 1
+                elif amount == 0:
+                    await ctx.message.add_reaction('\U0000274C')
+                    return await ctx.message.channel.send("You do not have enough coins to buy any tickets, sorry!")
+                else:
+                    break
+            if not user_tickets:
+                if amount > giveaway_info[1]:
+                    amount = giveaway_info[1]
+                self.c.execute('''INSERT OR IGNORE INTO entries VALUES (?, ?, ?)''', (giveaway_name, user_id, amount))
+                self.connection.commit()
+                print(self.c.execute('''SELECT * FROM entries ''').fetchall())
+            else:
+                if int(user_tickets[0]) >= giveaway_info[1]:
+                    await ctx.message.add_reaction('\U0000274C')
+                    return await ctx.message.channel.send("You already have the max number of tickets for this giveaway!")
+                if (amount + user_tickets[0]) > giveaway_info[1]:
+                    amount = int(giveaway_info[1])
+                self.c.execute('''UPDATE entries SET tickets = tickets + ? WHERE name=? AND id=?''', (amount, giveaway_name, user_id))
+                self.connection.commit()
+            self.c.execute('''UPDATE users SET xp = xp - ? WHERE id=?''', (total_cost, user_id))
+            self.connection.commit()
+            user_tickets = self.c.execute('''SELECT tickets FROM entries WHERE name=? AND id=?''', (giveaway_name, user_id)).fetchone()
+            entries = self.c.execute('''SELECT tickets FROM entries WHERE name=?''', (giveaway_name,)).fetchall()
+            num_tickets = 0
+            for ticket in entries:
+                num_tickets = num_tickets + int(ticket[0])
+            await ctx.message.channel.send("You bought **" + str(amount) + "** tickets for **" + str(giveaway_name) + "**.\nYou have **" + str(user_tickets[0]) + "** tickets for this giveaway. (Max of **" + str(giveaway_info[1]) + "**)\nThere are **"+ str(num_tickets) + "** entries so far in this giveaway.")
+            return await ctx.message.add_reaction('\U00002705')
         else:
             return await ctx.message.add_reaction('\U0000274C')
 
@@ -117,7 +219,7 @@ class Currency(commands.Cog, name="Currency"):
 
         # Adds a set amount of coins to a user
         if command.lower() == 'give' and len(args) == 2:
-            user_id = args[0].replace('<@', '').replace('>', '')
+            user_id = args[0].replace('<@', '').replace('!','').replace('>', '')
             if self.c.execute('''SELECT 1 FROM users WHERE id=?''', (user_id,)).fetchone():
                 self.c.execute('''UPDATE users SET xp = xp+? WHERE id=?''', (args[1], user_id))
                 self.connection.commit()
@@ -127,7 +229,7 @@ class Currency(commands.Cog, name="Currency"):
 
         # Removes a set amount of coins from a user
         elif command.lower() == 'remove' and len(args) == 2:
-            user_id = args[0].replace('<@', '').replace('>', '')
+            user_id = args[0].replace('<@', '').replace('!','').replace('>', '')
             if self.c.execute('''SELECT 1 FROM users WHERE id=?''', (user_id,)).fetchone():
                 self.c.execute('''UPDATE users SET xp = xp-? WHERE id=?''', (args[1], user_id))
                 self.connection.commit()
@@ -137,7 +239,7 @@ class Currency(commands.Cog, name="Currency"):
 
         # Sets a users balance to a certain amount
         elif command.lower() == 'set' and len(args) == 2:
-            user_id = args[0].replace('<@', '').replace('>', '')
+            user_id = args[0].replace('<@', '').replace('!','').replace('>', '')
             if self.c.execute('''SELECT 1 FROM users WHERE id=?''', (user_id,)).fetchone():
                 self.c.execute('''UPDATE users SET xp=? WHERE id=?''', (args[1], user_id))
                 self.connection.commit()
@@ -147,7 +249,7 @@ class Currency(commands.Cog, name="Currency"):
 
         # Resets a users balance to 0
         elif command.lower() == 'reset' and len(args) == 1:
-            user_id = args[0].replace('<@', '').replace('>', '')
+            user_id = args[0].replace('<@', '').replace('!','').replace('>', '')
             if self.c.execute('''SELECT 1 FROM users WHERE id=?''', (user_id,)).fetchone():
                 self.c.execute('''UPDATE users SET xp=0 WHERE id=?''', (user_id,))
                 self.connection.commit()
@@ -163,7 +265,10 @@ class Currency(commands.Cog, name="Currency"):
 
         error = getattr(error, "original", error)
         if isinstance(error, MissingPermissions) or isinstance(error, MissingRequiredArgument):
+            print(str(error))
             return await ctx.message.add_reaction('\U0000274C')
+        if isinstance(error, commands.CommandOnCooldown):
+            return await ctx.message.add_reaction('\U0001F552')
         else:
             raise error
 
